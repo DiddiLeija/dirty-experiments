bb2a165fa0ba794de3f1ccb49d92b51437efb7a5
Diego Ramirez <dr01191115@gmail.com>
2021-09-06 12:10:56 -0500

    Update example.cpp
    Extend the example.

--- cpp/io-example/example.cpp
+++ cpp/io-example/example.cpp
@@ -8,7 +8,7 @@ takes a numeric value, and then print it.
 (I tried this with a C++ editor, so tell me if this fails on another cases).
 */
 
-// This library should be everywhere
+// This library should be included on any C++ distribution, right?
 #include<iostream>
 
 // use a namespace to print things
@@ -18,14 +18,18 @@ using namespace std;
 int main(){
 	
 	// define values
-	float number;
+	float float_number;
+	int int_number;
 	
-	// store an input
+	// store input
 	cout<<"Introduce a floating number:\n> ";
-	cin>>number;
+	cin>>float_number;
+	cout<<"\nIntroduce an integer:\n>";
+	cin>>int_number;
 	
-	// retrieve
-	cout<<"\nNumber introduced:\n"<<number;
+	// retrieve the results
+	cout<<"\nFloat introduced:\n"<<float_number;
+	cout<<"\nInteger introduced:\n"<<int_number<<endl; // the last value should have an extra "endline"
 	
 	return 0;  // return nothing
 }
