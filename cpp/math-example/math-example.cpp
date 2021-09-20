3a5fdd5e0c3104c58166562fc2996318d6eaaa0d
Diego Ramirez <dr01191115@gmail.com>
2021-09-20 08:54:30 -0500

    Update math-example.cpp
    Add more comments.

--- cpp/math-example/math-example.cpp
+++ cpp/math-example/math-example.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
-#include<math.h>
+#include<math.h> // this is the "math" library
 
 using namespace std;
 
 int main() {
   // create the variables
-	float a, b, c, d;
+  float a, b, c, d;
   float result=0;
-  
+
   // ask for the values
   cout<<"Introduce 'a': "; cin>>a;
   cout<<"Introduce 'b': "; cin>>b;
   cout<<"Introduce 'c': "; cin>>c;
   cout<<"Introduce 'd': "; cin>>d;
-  
-  // make the operation
+
+  // make the operation and save it
+  // at "result"
   result = (a + pow(b * a), 3) - (sqrt((c / d)));
-  
+
   // show the result
   cout<<endl<<"Result: "<<result<<endl;
-  
-	return 0; // exit code 0
+
+  return 0; // exit code 0
 }
