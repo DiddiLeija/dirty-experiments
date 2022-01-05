c80b0b3d80fe7b1a3d1febeac4a75b4973c6c653
Diego Ramirez <dr01191115@gmail.com>
2022-01-05 14:13:04 -0600

    Create lists.kt
    A Kotlin example of lists and mutable lists.

---
+++ kotlin/lists/lists.kt
@@ -0,0 +1,70 @@
+/*
+ * Quick example of Lists and MutableLists
+ * =======================================
+ * 
+ * This demo shows the possible stuff to
+ * work with lists and mutable lists.
+ * */
+
+fun lists() {
+    // Kotlin lists are unmutable. They are
+    // somehow similar to Python tuples.
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+
+    // Access elements of the list
+    println("First element: ${numbers[0]}")
+    println("Second element: ${numbers[1]}")
+    println("Last index: ${numbers.size - 1}")
+    println("Last element: ${numbers[numbers.size - 1]}")
+    println("First: ${numbers.first()}")
+    println("Last: ${numbers.last()}")
+
+    // Use the contains() method
+    println("Contains 4? ${numbers.contains(4)}")
+    println("Contains 7? ${numbers.contains(7)}")
+}
+
+fun mutables() {
+    // Mutable lists can be modified freely. They are
+    // like the Python lists.
+    val entrees = mutableListOf<String>()
+    println("Entrees: $entrees")
+
+    // Add individual items using add()
+    println("Add noodles: ${entrees.add("noodles")}")
+    println("Entrees: $entrees")
+    println("Add spaghetti: ${entrees.add("spaghetti")}")
+    println("Entrees: $entrees")
+
+    // Add a list of items using addAll()
+    val moreItems = listOf("ravioli", "lasagna", "fettuccine")
+    println("Add list: ${entrees.addAll(moreItems)}")
+    println("Entrees: $entrees")
+
+    // Remove an item using remove()
+    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
+    println("Entrees: $entrees")
+    println("Remove item that doesn't exist: ${entrees.remove("rice")}")
+    println("Entrees: $entrees")
+
+    // Remove an item using removeAt() with an index
+    println("Remove first element: ${entrees.removeAt(0)}")
+    println("Entrees: $entrees")
+
+    // Clear out the list
+    entrees.clear()
+    println("Entrees: $entrees")
+
+    // Check if the list is empty
+    println("Empty? ${entrees.isEmpty()}")
+}
+
+fun main() {
+    // The main function. Just run and separate
+    // the 2 functions created above...
+    lists()
+    println("===================")
+    mutables()
+}
